8dd6b7960a3c244c34dc48a3b8fd8e211ce4d885
Sonia <sonia_var@hotmail.com>
2021-10-01 10:40:18 -0400

    Third try

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,3 +1,4 @@
-export const baseUrl = "http://3.15.175.42/";
+export const baseUrl = "http://13.58.5.41/";
 
 //every time we stop and restart an instance the ip changes, so we have to copy the new ip here
+//start bash terminal from desktop, then cd to ssh and over there, paste the ssh -i
